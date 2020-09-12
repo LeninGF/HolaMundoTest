83f695c61a19edb6a503c721e2c21cf23b9f3fd5
LeninGF <enteatenea@gmail.com>
2020-09-11 22:33:37 -0500

    archivo para tarea

---
+++ repaso_for.py
@@ -0,0 +1,11 @@
+# Lista de Tareas con For
+
+# Imprimir los numeros enteros entre 0 - 10
+for i in range(5):
+    print(i, end=',')
+print()
+
+numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+for numero in numeros:
+    print(numero, end=',')
+
